e5bbb193823dc181cb428cae75ffa4522d8e4a53
Artur Vincho <vincho.artur@gmail.com>
2024-05-07 12:59:55 +0300

    add registration with hashed password, change login logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,13 @@ import { UserModule } from './app/modules/user/user.module';
 import { db_config } from './configuration/db_mysql';
 import { DatabaseModule } from './app/modules/database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from './validation/env.validation';
 import { AuthModule } from './app/modules/auth/auth.module';
+import { WebsocketModule } from './app/modules/calls/websoket/websocket.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(db_config),
-    UserModule, DatabaseModule, AuthModule,
-    ConfigModule.forRoot({
-
-    })
+    UserModule, DatabaseModule, AuthModule, WebsocketModule,
+    ConfigModule.forRoot({}),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,44 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import * as cors from 'cors';
+import * as http from 'http';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap(): Promise<void> {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.use(cors())
+
+  // Настраиваем CORS для HTTP-запросов
+  app.enableCors({
+    origin: "*", // Разрешаем запросы с любого источника
+    methods: ["GET", "POST"], // Разрешаем только методы GET и POST
+  });
+
+  // Создаем HTTP-сервер
+  const httpServer = http.createServer(server);
+  const io = require('socket.io')(httpServer, {
+    cors: {
+      origin: "*", // Разрешаем запросы с любого источника
+      methods: ["GET", "POST"], // Разрешаем только методы GET и POST
+    }
+  });
+
+  // middleware для обработки запросов к пути /socket.io/
+  io.use((socket, next) => {
+    // ваша логика обработки здесь
+    console.log("Incoming Socket.IO request:", socket.handshake.url);
+    next();
+  });
+  // Добавляем маршрут для обработки запросов к /socket.io/
+  server.get('/socket.io/*', (req, res) => {
+    // Здесь может быть ваша логика обработки запросов к /socket.io/
+    // Например, отправка index.html или 404 ошибки
+    res.status(200).send('Not Found');
+  });
+
+  // Создаем экземпляр socket.io и привязываем его к HTTP-серверу
+  app.useWebSocketAdapter(new IoAdapter(httpServer));
+
   await app.listen(3001);
 }
 bootstrap();
