f76b4e32cfd717e67a1132e3ce79d47150ce0e35
Artur Vincho <vincho.artur@gmail.com>
2024-05-18 18:16:39 +0300

    work socket

--- src/app/modules/calls/websoket/websocket.module.ts
+++ src/app/modules/calls/websoket/websocket.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WebsocketGateway } from './websocketGateway';
+import { ActivatedServices } from '../services/activated.services';
 
 @Module({
-  providers: [WebsocketGateway],
+  providers: [WebsocketGateway, ActivatedServices],
   exports: [WebsocketGateway], // Добавляем экспорт WebsocketGateway
 })
 export class WebsocketModule {}
--- src/app/modules/calls/websoket/websocketGateway.ts
+++ src/app/modules/calls/websoket/websocketGateway.ts
@@ -1,14 +1,39 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import * as stream from 'node:stream';
+import { ActivatedServices } from '../services/activated.services';
 
 @WebSocketGateway({ namespace: 'calls', cors: {origin: '*'} })
 export class WebsocketGateway {
   @WebSocketServer()
   server: Server;
 
+  constructor(private readonly activateService: ActivatedServices) {
+  }
+
+  private activeUsers: Map<string, any> = new Map();
+
   @SubscribeMessage('calls')
-  handleEvent(@MessageBody() data: string): string {
-    return data;
+  handleEvent(@MessageBody() data: any, @ConnectedSocket() client: Socket): string {
+    const { userId, streamId, tracks } = data;
+    this.activateService.activatedUsers = {userId, stream: { streamId, tracks }}
+    this.activeUsers.set(userId, client);
+    this.activeUsers.forEach((otherClient, otherUserId) => {
+      if (otherUserId !== userId) {
+        const streamInfo = this.activateService.activatedUsers.get(otherUserId);
+        otherClient.emit('remoteStream', {userId, streamId: streamInfo.streamId, tracks: streamInfo.tracks})
+      }
+    })
+    return '';
+  }
+
+  @SubscribeMessage('disconnect')
+  handleDisconnect(@ConnectedSocket() client: Socket): void {
+    this.activeUsers.forEach((otherClient, otherUserId) => {
+      if (otherClient === client) {
+        this.activeUsers.delete(otherUserId);
+      }
+    })
   }
 
 }
