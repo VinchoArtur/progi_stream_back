0dfdacae51af16743e494a7cd2bb738759d48aef
Artur Vincho <vincho.artur@gmail.com>
2024-05-18 16:27:56 +0300

    work socket

--- src/app/modules/calls/websoket/websocketGateway.ts
+++ src/app/modules/calls/websoket/websocketGateway.ts
@@ -1,47 +1,14 @@
-import { WebSocketGateway, OnGatewayConnection, WebSocketServer, OnGatewayInit, SubscribeMessage, MessageBody } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
-export class WebsocketGateway implements OnGatewayConnection, OnGatewayInit {
+@WebSocketGateway({ namespace: 'calls', cors: {origin: '*'} })
+export class WebsocketGateway {
   @WebSocketServer()
   server: Server;
 
-  clients: Set<Socket> = new Set();
-
-  afterInit() {
-    console.log('WebSocket server initialized');
+  @SubscribeMessage('calls')
+  handleEvent(@MessageBody() data: string): string {
+    return data;
   }
 
-  handleConnection(client: Socket) {
-    console.log('Client connected');
-    this.clients.add(client);
-
-    // Обработка отключения клиента
-    client.on('disconnect', () => {
-      console.log('Client disconnected');
-      this.clients.delete(client);
-    });
-  }
-
-  @SubscribeMessage('call')
-  handleCall(@MessageBody() data: any, client: Socket) {
-    console.log('Received call from client:', data);
-    // Рассылаем данные потока клиента всем остальным клиентам
-    this.clients.forEach(ws => {
-      if (ws !== client) {
-        ws.emit('stream', data);
-      }
-    });
-  }
-
-  @SubscribeMessage('iceCandidate')
-  handleIceCandidate(@MessageBody() data: any, client: Socket) {
-    console.log('Received ICE candidate from client:', data);
-    // Пересылаем данные ICE кандидата всем остальным клиентам
-    this.clients.forEach(ws => {
-      if (ws !== client) {
-        ws.emit('iceCandidate', data);
-      }
-    });
-  }
 }
